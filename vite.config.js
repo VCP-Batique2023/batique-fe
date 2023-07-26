3c2a195a52bda6ce39494066dbc1769a4995892c
hsymicm <ichorvfx@gmail.com>
2023-07-26 19:14:37 +0700

    init

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url';
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: `/`,
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+            return `assets/${extType}/[hash][extname]`;
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        // manualChunks: {
+        //   vendor: ['react', 'react-router-dom', 'react-dom'],
+        //   firebase: ['firebase/auth', 'firebase/firestore', 'firebase/storage'],
+        // },
+      },
+    },
+  },
+});
