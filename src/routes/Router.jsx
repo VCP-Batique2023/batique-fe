1f1afa262993dabe9dc0e52872b435f9c292413d
hsymicm <ichorvfx@gmail.com>
2023-08-07 00:59:13 +0700

    Configure not found route, loading bar, scroll top

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,9 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import DocumentTitle from '@/routes/Title';
+import LoadingBar from '@/components/LoadingBar';
+import ScrollTop from '@/components/ScrollTop';
+import NotFound from '@/pages/NotFound';
 import '@/assets/style/index.css';
 
 const Home = lazy(() => import('@/pages/Home'));
@@ -8,13 +11,29 @@ const Home = lazy(() => import('@/pages/Home'));
 export default function Router() {
   return (
     <>
-      <Suspense fallback={<main>Loading...</main>}>
+      <ScrollTop />
+      <Suspense
+        fallback={
+          <main>
+            <LoadingBar />
+          </main>
+        }
+      >
         <Routes>
           <Route
+            exact
             path="/"
             element={
-              <DocumentTitle title="Selamat datang di Batique">
+              <DocumentTitle title="Selamat datang - Batique">
                 <Home />
+              </DocumentTitle>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <DocumentTitle title="Halaman tidak ada - Batique">
+                <NotFound />
               </DocumentTitle>
             }
           />
