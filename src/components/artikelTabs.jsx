5f18558d0d8b8c7b8d99c9ee906a06b4f9ba5d9e
tiarapus <puspitatiara14@gmail.com>
2023-08-08 09:27:58 +0700

    update tabs

--- src/components/artikelTabs.jsx
+++ src/components/artikelTabs.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '@/assets/style/artikelTabs.css';
 import '@/assets/style/artikelCard.css';
 import '@/assets/style/Layout.css';
 
 import ArtikelCard from './artikelCard';
-import Button from './Button';
+import Dropdown from './dropdown';
 export default function artikelNav() {
-    const categories = [
-        "History",
-        "DIY",
-        "Filter"
+  
+    const options = [
+        {  text: 'History' },
+        {  text: 'DIY' },
+        { text: 'Filter' },
+        
     ];
     const artikel = [
         {
@@ -35,39 +37,127 @@ export default function artikelNav() {
             img:"https://source.unsplash.com/random/367x217/?batik",
             link : "#experience",
             
+            
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "History",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#home",
+           
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "DIY",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#about",
+            
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "Filter",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#experience",
+            
+            
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "History",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#home",
+           
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "DIY",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#about",
+            
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "Filter",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#experience",
+            
+            
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "History",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#home",
+           
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "DIY",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#about",
+            
+        },
+        {
+            title:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            excerpt:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tristique luctus enim, sit amet gravida nunc vestibulum eget. Integer ullamcorper tincidunt libero, non commodo quam convallis ut.",
+            category : "Filter",
+            img:"https://source.unsplash.com/random/367x217/?batik",
+            link : "#experience",
+            
+            
         }
     ]
-    const [activeCategory, setActiveCategory] = useState(categories[0]);
-
-    const filteredArtikel = artikel.filter(item => item.category === activeCategory);
-
+    const [activeCategory, setActiveCategory] = useState(null);
+    const [isSticky, setIsSticky] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollPosition = window.scrollY;
+          setIsSticky(scrollPosition > 90); 
+        };
+        
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    
+    const filteredArtikel = activeCategory ? artikel.filter(item => item.category === activeCategory)
+    : artikel;
     const handleCategoryChange = (category) => {
         setActiveCategory(category);
     }
 
     return (
         <div className="container">
-            <div className="nav-container">
-                <div className="nav-tabs">
-                <ul>
-                    {categories.map((category, index) => (
-                        <li
-                            className='main'
-                        >
-                        <Button
-                        className={`btn-outlined-primary ${activeCategory === category ? 'active' : ''}`}
-                        key={index}
-                        onClick={() => handleCategoryChange(category)}
-                        variant="outlined"
-                        >
-                            {category}
-                        </Button>
-
-                        </li>
-                    ))}
+            {/* navigasi tabs */}
+            <div className={`nav-tabs ${isSticky ? 'sticky' : ''}`}>
+                <ul className='dropdown'>
+                    <li>
+                        <Dropdown options={options} onItemClick={(category) => handleCategoryChange(category || null)} />
+                    </li>
+                </ul>   
+                <ul className='title'>
+                    <li>
+                        <span className='nav-header'>Jelajahi Artikel Terbaru</span>
+                    </li>
                 </ul>
-                </div>
+                <ul>
+                    <li>
+                        <input type="text" placeholder="Search.."></input>
+                    </li>
+                </ul>
+                
             </div>
+            {/* card tabs */}
+            
             <ArtikelCard
                 artikel={filteredArtikel} 
                 categoryColor="secondary" 
