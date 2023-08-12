ac3c865d70e25d710868b51676fda77703cae1b7
hsymicm <ichorvfx@gmail.com>
2023-08-12 20:42:51 +0700

    Add Sign Up Sign In Routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,6 +6,8 @@ import '@/assets/style/index.css';
 
 const NotFound = lazy(() => import('@/pages/NotFound'))
 const Home = lazy(() => import('@/pages/Home'));
+const SignUp = lazy(() => import('@/pages/SignUp'));
+const SignIn = lazy(() => import('@/pages/SignIn'));
 
 export default function Router() {
   return (
@@ -24,6 +26,24 @@ export default function Router() {
             element={
               <DocumentTitle title="Selamat datang - Batique">
                 <Home />
+              </DocumentTitle>
+            }
+          />
+          <Route
+            exact
+            path="/daftar"
+            element={
+              <DocumentTitle title="Daftar Akun - Batique">
+                <SignUp />
+              </DocumentTitle>
+            }
+          />
+          <Route
+            exact
+            path="/masuk"
+            element={
+              <DocumentTitle title="Masuk - Batique">
+                <SignIn />
               </DocumentTitle>
             }
           />
