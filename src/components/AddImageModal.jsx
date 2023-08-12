d754c54f299669906060f0e135f3996c9be26caf
calvindany <calvindany1102@gmail.com>
2023-08-12 16:25:45 +0700

    Finishing Add Image Modal Component

--- src/components/AddImageModal.jsx
+++ src/components/AddImageModal.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/no-children-prop */
 import Button from '@/components/Button';
+import ImageIcon from '@/assets/img/6.png'
 import '@/assets/style/ImageGrid.css';
 
 export default function ImageModal({ onClick, show }) {
@@ -9,23 +10,15 @@ export default function ImageModal({ onClick, show }) {
         className="detailPostContainer"
         style={{ zIndex: show, opacity: show }}
       >
-        <div className="detailPost">
+        <div className="detailPost addPost">
           <div className="leftContent">
-            <svg
-              style={{ backgroundColor: 'transparent' }}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="solid"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-              />
-            </svg>
+            <label htmlFor="fileUpload" className='fileUpload' style={{
+              backgroundColor: 'transparent', width: '100%'
+            }}>
+              <img src={ImageIcon} alt="" style={{ width: '30%', backgroundColor: 'transparent' }} />
+              <input id='fileUpload' type="file" hidden style={{ backgroundColor: 'transparent' }} />
+            </label>
+
           </div>
           <div className="rightContent">
             <div className="profileInformation">
