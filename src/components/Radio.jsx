528b86d32d9f593d328b37cbd169136344fc7e1f
hsymicm <ichorvfx@gmail.com>
2023-08-12 20:41:59 +0700

    Update Input Components

--- src/components/Radio.jsx
+++ src/components/Radio.jsx
@@ -1,13 +1,23 @@
-export default function Radio({ name, label, checked }) {
-    return (
-        <label>
-            <input
-                type="radio"
-                id={label.toLowerCase()}
-                name={name}
-                value={label.toLowerCase()}
-            />
-            {label}
-        </label>
-    )
+export default function Radio({ name, label, value, setValue, required = false }) {
+  return (
+    <label
+      style={{
+        display: 'flex',
+        gap: 6,
+        accentColor: '#372b22',
+        lineHeight: '75%',
+      }}
+    >
+      <input
+        type="radio"
+        id={label.toLowerCase()}
+        name={name}
+        value={label.toLowerCase()}
+        checked={label.toLowerCase() === value}
+        required={required}
+        onChange={(e) => setValue(e.currentTarget.value)}
+      />
+      {label}
+    </label>
+  );
 }
