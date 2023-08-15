de68117ea23026feaf162dca00861e06d3e0312b
Calvin Danyalson <calvindany1102@gmail.com>
2023-08-15 15:57:19 +0700

    Change Device

--- src/pages/Feeds.jsx
+++ src/pages/Feeds.jsx
@@ -8,234 +8,19 @@ import ImageModal from '@/components/ImageModal';
 import AddImageModal from '@/components/AddImageModal';
 import Button from '@/components/Button';
 
-import { collection, doc, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot } from 'firebase/firestore';
 import { db } from '@/modules/firebase_config';
 
 import GaleryHeader from '@/assets/img/1.jpg';
 
-// Image Dummy Data
-const feedsDummyList = [
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/30.jpg',
-    like: 10,
-    createdAt: new Date('11-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/31.jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/31.jpg',
-    like: 100,
-    createdAt: new Date('02-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/33.jpg',
-    like: 90,
-    createdAt: new Date('11-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/34.jpg',
-    like: 10,
-    createdAt: new Date('01-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/35.jpg',
-    like: 10,
-    createdAt: new Date('10-07-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/38.jpg',
-    like: 53,
-    createdAt: new Date('12-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/39.jpg',
-    like: 53,
-    createdAt: new Date('12-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/40.jpg',
-    like: 31,
-    createdAt: new Date('11-07-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(2).jpg',
-    like: 10,
-    createdAt: new Date('01-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(2).jpg',
-    like: 10,
-    createdAt: new Date('01-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(2).jpg',
-    like: 10,
-    createdAt: new Date('01-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(3).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(4).jpg',
-    like: 34,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(5).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(6).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(7).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(8).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(9).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(10).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(11).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(12).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(13).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(14).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(15).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(16).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(17).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(18).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(19).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-  {
-    caption:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    url: 'https://storage.googleapis.com/batique-images/a%20(20).jpg',
-    like: 100,
-    createdAt: new Date('10-08-2023'),
-  },
-];
-
 export default function Galery() {
-  // Fetch data from firebase
-
   const [screenWidth, setScreenWidth] = useState(
     window.innerWidth >= 992 ? '60vh' : '30vh'
   );
   const [feedsList, setFeedsList] = useState([]);
 
   const [activeSort, setActiveSort] = useState('default');
+  const { ref: targetButtonRef, inView: targetButtonIsVisible } = useInView();
   const [showModalDetailPost, setShowModalDetailPost] = useState(-1);
   const [showModalAddPost, setShowModalAddPost] = useState(-1);
   const [postData, setPostData] = useState({});
@@ -320,8 +105,8 @@ export default function Galery() {
         viewBox="0 0 25 25"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="w-6 h-6"
-        style={{ backgroundColor: 'transparent' }}
+        width={24}
+        height={24}
       >
         <path
           strokeLinecap="round"
@@ -363,8 +148,8 @@ export default function Galery() {
         children={plusIcon()}
         style={{
           borderRadius: '50%',
-          width: '5vh',
-          height: '5vh',
+          width: '7vh',
+          height: '7vh',
           position: 'fixed',
           right: '10vh',
           bottom: '10vh',
@@ -372,6 +157,7 @@ export default function Galery() {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          padding: 4,
         }}
         onClick={triggerShowModalAddPost}
       />
