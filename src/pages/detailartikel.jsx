16466b63117bf68f9135a7931d9063e3e70e7942
hsymicm <ichorvfx@gmail.com>
2023-08-20 20:46:55 +0700

    fix non JSX atributes #icam

--- src/pages/detailartikel.jsx
+++ src/pages/detailartikel.jsx
@@ -1,39 +1,36 @@
 import { motion } from 'framer-motion';
 import { useLocation, useParams } from 'react-router-dom';
-// import { formatDistanceToNow, format } from 'date-fns';
-import "@/assets/style/detailArtikel.css"
-import { Timestamp } from 'firebase/firestore';
-import { useNavigate } from "react-router-dom";
+import '@/assets/style/detailArtikel.css';
+import { useNavigate } from 'react-router-dom';
 import GridLoader from 'react-spinners/GridLoader';
 
 export default function DetailArtikel() {
   const location = useLocation();
   const { index } = useParams();
   const artikel = location.state?.artikel;
-  const naigate = useNavigate();
-  
+  const navigate = useNavigate();
 
   const navigateToArtikel = () => {
-      naigate("/artikel"); // Replace with the actual route for the "artikel" page
+    navigate('/artikel'); // Replace with the actual route for the "artikel" page
   };
 
   // if (!artikel || !artikel[index]) {
   //   return <div style={{ height:"100vh",margin:"auto"}}>Loading...</div>;
-    
+
   // }
 
   function formatTimestamp(timestamp) {
     if (!timestamp) {
-      return ''; 
+      return '';
     }
-    
+
     const createdAt = new Date(timestamp.seconds * 1000);
-    return createdAt.toLocaleDateString(); 
+    return createdAt.toLocaleDateString();
   }
 
   const selectedCard = artikel[index];
   // const formatRelativeTime = (timestamp) => {
-  //   const createdAt = new Date(timestamp * 1000); 
+  //   const createdAt = new Date(timestamp * 1000);
   //   const currentDate = new Date();
   //   const yearDifference = currentDate.getFullYear() - createdAt.getFullYear();
   //   const relativeTime = formatDistanceToNow(createdAt, { addSuffix: true });
@@ -42,57 +39,63 @@ export default function DetailArtikel() {
   //       return `${relativeTime}, ${formattedYear}`;
   //   }
 
-//     return relativeTime;
-// };
+  //     return relativeTime;
+  // };
 
   return (
     <>
-    {
-      selectedCard ? (
-          <motion.main
-            className="container-detail"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-          >
-          <div style={{ width:"30px"}}>
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                fill="none" viewBox="0 0 24 24" 
-                stroke-width="1.5" stroke="currentColor" 
-                class="w-6 h-6"
-                onClick={navigateToArtikel}
-                style={{ cursor:"pointer" }}
-                >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
+      {selectedCard ? (
+        <motion.main
+          className="container-detail"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <div style={{ width: '30px' }}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+              onClick={navigateToArtikel}
+              style={{ cursor: 'pointer' }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75"
+              />
             </svg>
           </div>
 
           <div className="title">
             <h1>{selectedCard.title}</h1>
           </div>
-          <div className='time'>
-            <span >{formatTimestamp(selectedCard.createdat)}</span>
+          <div className="time">
+            <span>{formatTimestamp(selectedCard.createdat)}</span>
           </div>
           <img src={selectedCard.img} alt="" className="cover" />
           <div className="content">
             {/* Check if selectedCard.content contains <br> tags */}
             {selectedCard.content.includes('<br>') ? (
-                <p dangerouslySetInnerHTML={{ __html: selectedCard.content }} />
+              <p dangerouslySetInnerHTML={{ __html: selectedCard.content }} />
             ) : (
-                <p>{selectedCard.content.replace(/\n/g, "<br>")}</p>
+              <p>{selectedCard.content.replace(/\n/g, '<br>')}</p>
             )}
           </div>
-      </motion.main>
-      ):(
-        <div style={{
-          display: 'flex',
-          justifyContent: 'center',
-          padding: '68px 0'
-        }}>
+        </motion.main>
+      ) : (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            padding: '68px 0',
+          }}
+        >
           <GridLoader color="#372B22" />
         </div>
-      )
-    }
+      )}
     </>
-    
   );
 }
